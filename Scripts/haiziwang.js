79a1d3a229355656b2bdf16889e1a31de75467d5
luojing <luojingam@163.com>
2024-06-28 13:43:32 +0800

    haiziwang1

--- Scripts/haiziwang.js
+++ Scripts/haiziwang.js
@@ -1,3 +1,18 @@
+/**********************************************
+
+ > 应用名称：孩子王
+ > 更新时间：2024-06-28
+ > 脚本说明：去除开屏广告
+
+
+ [rewrite_local]
+ ^https?:\/\/cms.cekid.com\/publish\/998\/2023applaunchconfig.json url script-response-body https://raw.githubusercontent.com/luojingam/QX/master/Scripts/haiziwang.js
+
+ [mitm]
+ hostname = cms.cekid.com
+
+ **********************************************/
+
 let obj = JSON.parse($response.body);
 obj.data.launch_ad = [];
 $done({body: JSON.stringify(obj)});
